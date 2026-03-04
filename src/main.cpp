b15a277e0322aefdd47feabcf4fc9333ed8489b1
Shreyas <shreyasxcoding@gmail.com>
2026-03-04 15:28:21 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include<algorithm>
 
 int main() {
   // Flush after every std::cout / std:cerr 
   // usually it waits until the buffer is full
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::vector<std::string> type_value={"echo", "exit", "type"};
 
   // TODO: Uncomment the code below to pass the first stage
   while(true){
@@ -19,6 +23,12 @@ int main() {
     //echo command
     if(command.substr(0,5)=="echo "){
       std::cout<<command.substr(5)<<std::endl;
+    }else if(command.substr(0,5)=="type "){
+      if(std::find(type_value.begin(),type_value.end(),command.substr(5))!=type_value.end()){
+        std::cout<<command.substr(5)<<" is a builtin command"<<std::endl;
+      }else{
+        std::cout<<command<<": command not found"<<std::endl;
+      }
     }else{
       std::cout<<command<<": command not found"<<std::endl;
     }
