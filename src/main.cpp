5b19687abae7be1694c90049acd34fcf2f364061
Shreyas <shreyasxcoding@gmail.com>
2026-03-14 15:54:15 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,65 @@
 #include <string>
 #include <vector>
 #include<algorithm>
+#include <sstream>
+#include <unistd.h>
 
 int main() {
   // Flush after every std::cout / std:cerr 
   // usually it waits until the buffer is full
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string command;
+  std::string line;
 
   std::vector<std::string> type_value={"echo", "exit", "type"};
 
   // TODO: Uncomment the code below to pass the first stage
   while(true){
     std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
+    std::getline(std::cin, line);
+    std::stringstream ss(line);
+    ss>>command;
 
     //to exit REPL
     if(command=="exit") break;
 
     //echo command
-    if(command.substr(0,5)=="echo "){
-      std::cout<<command.substr(5)<<std::endl;
-    }else if(command.substr(0,5)=="type "){
-      if(std::find(type_value.begin(),type_value.end(),command.substr(5))!=type_value.end()){
-        std::cout<<command.substr(5)<<" is a shell builtin"<<std::endl;
-      }else{
+    if(command=="echo"){
+      std::string word;
+      while(ss>>word){
+        std::cout<<word<<" ";
+      }
+      std::cout<<"\n";
+    }
+    //type command
+    else if(command=="type"){
+      bool found=false;
+      std::string command_to_know;
+      ss>>command_to_know;
+
+      if(std::find(type_value.begin(),type_value.end(),command_to_know)!=type_value.end()){
+        std::cout<<command_to_know<<" is a shell builtin"<<std::endl; 
+        found=true;
+      }
+      if(!found){
+        std::string path_env=std::getenv("PATH");
+        std::stringstream ss_path(path_env);
+        std::string path;
+        while(std::getline(ss_path,path, ':')){
+          std::string full_path=path+'/'+command_to_know;
+          if(access(full_path.c_str(),X_OK)==0){
+            std::cout<<command_to_know<<"is"<<full_path<<std::endl;
+            found=true;
+            break;
+          }
+        }
+      }
+      if(!found){
         std::cout<<command.substr(5)<<": not found"<<std::endl;
       }
-    }else{
+    }
+    else{
       std::cout<<command<<": command not found"<<std::endl;
     }
   }
